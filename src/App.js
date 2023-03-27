9719b7896b3b6142758f6ad679316645a9c72110
Chris Jagernath <jagernat@sheridancollege.ca>
2023-03-27 13:46:59 -0400

    fixed toast showing up constantly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import FileUpload from "./FileUpload";
-import { Button, Input, Textarea, Center, Text, Heading, useToast, Container } from '@chakra-ui/react'
+import {
+  Button,
+  Input,
+  Textarea,
+  Center,
+  Text,
+  Heading,
+  useToast,
+  Container,
+} from "@chakra-ui/react";
 
 function App() {
   const [script, setScript] = useState("");
@@ -10,7 +19,8 @@ function App() {
   const [title, setTitle] = useState("");
   const [presId, setPresId] = useState("");
 
-  let url = "https://docs.google.com/presentation/d/" + presId + "/edit#slide=id.p";
+  let url =
+    "https://docs.google.com/presentation/d/" + presId + "/edit#slide=id.p";
 
   //only works for txt files
   const handleFileUploaded = (file) => {
@@ -19,20 +29,19 @@ function App() {
     reader.onload = () => {
       const contents = reader.result;
       setScript(contents);
-    }
+    };
 
     reader.readAsText(file);
-
   };
 
   const handleChange = (event) => {
     setInput(event.target.value);
     setScript(event.target.value);
-  }
+  };
 
-  const handleChangeTitle= (event) => {
+  const handleChangeTitle = (event) => {
     setTitle(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -40,54 +49,58 @@ function App() {
     setInput("");
     setTitle("");
     handleSummarize();
-
   };
 
   const handlePaste = (event) => {
     const pastedText = event.clipboardData.getData("text");
-    const newValue = input.substring(0, event.target.selectionStart) + pastedText + input.substring(event.target.selectionEnd);
+    const newValue =
+      input.substring(0, event.target.selectionStart) +
+      pastedText +
+      input.substring(event.target.selectionEnd);
     setInput(newValue);
     setScript(newValue);
-  }
+  };
 
   async function handleSummarize() {
     //makes http request to server on exposed endpoint
     const response = await fetch("http://localhost:5000/summarize", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        prompt: script
-      })
+        prompt: script,
+      }),
     });
     const aiResponse = await response.json();
-    //split the json reponse by dashes into an array 
+    //split the json reponse by dashes into an array
     console.log(aiResponse);
-    let pointsArray = aiResponse.data.text.split(':::').slice(1);
-    if (pointsArray.length%3 !== 0){
-      const remainder = 3 - pointsArray.length%3;
-      for (let q = 0;q < remainder; q++){
+    let pointsArray = aiResponse.data.text.split(":::").slice(1);
+    if (pointsArray.length % 3 !== 0) {
+      const remainder = 3 - (pointsArray.length % 3);
+      for (let q = 0; q < remainder; q++) {
         pointsArray.push("");
       }
     }
 
     setScript("");
     handleCreate(title, pointsArray);
- 
   }
 
-  async function handleCreate(title, summaryPoints){
+  async function handleCreate(title, summaryPoints) {
     //makes http request to create presentation
     console.log(title, summaryPoints);
     try {
-      const response = await fetch('http://localhost:5000/create-presentation', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, summaryPoints }),
-      });
+      const response = await fetch(
+        "http://localhost:5000/create-presentation",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, summaryPoints }),
+        }
+      );
       const presentation = await response.json();
       setPresId(presentation.data.presentationId);
 
@@ -95,54 +108,70 @@ function App() {
     } catch (error) {
       console.error(error);
     }
-
   }
 
-const toast = useToast();
+  const toast = useToast();
 
   return (
     <div>
-      <Container maxW={'700px'}>
-      <Center bg='black' h='100px' color='white'><Text fontSize='5xl'>Google Slides Automator</Text></Center><br />
-      <Center>
-      <FileUpload onFileUploaded={handleFileUploaded} />
-      </Center>
-      <br />
-      <Center><Heading>OR</Heading></Center><br />
-      <Center>
-      <Heading>Paste the script below</Heading><br />
-      </Center>
-      <form onSubmit={handleSubmit}>
-        <strong>Title: </strong>
-        <Input
-        type="text"
-        placeholder="Enter Title"
-        onChange={handleChangeTitle}
-        value={title}>
-        </Input><br /><br/>
-        <strong>Script: </strong>
-        <Textarea
-          type="text"
-          placeholder="Enter Script"
-          cols={80}
-          rows={10}
-          onChange={handleChange}
-          onPaste={handlePaste}
-          value={input} /><br />
-        <Button type="submit" disabled={input === ""} >Submit</Button>
-      </form>
-      <Center>
-      { (presId !== "") ? toast({
-          title: 'Hooray!!!',
-          description: `Presentation has been created! ${url}`,
-          status: 'success',
-          duration: 5000,
-          isClosable: true,
-        }) : null}
-      </Center>
+      <Container minW="700" bg="gray.700">
+        <Center bg="black" h="100px" color="white">
+          <Text fontSize="5xl">Google Slides Automator</Text>
+        </Center>
+        <br />
+        <Center>
+          <FileUpload onFileUploaded={handleFileUploaded} />
+        </Center>
+        <br />
+        <Center>
+          <Heading>OR</Heading>
+        </Center>
+        <br />
+        <Center>
+          <Heading>Paste the script below</Heading>
+          <br />
+        </Center>
+        <form onSubmit={handleSubmit}>
+          <strong>Title: </strong>
+          <Input
+            type="text"
+            placeholder="Enter Title"
+            onChange={handleChangeTitle}
+            value={title}
+          ></Input>
+          <br />
+          <br />
+          <strong>Script: </strong>
+          <Textarea
+            type="text"
+            placeholder="Enter Script"
+            cols={80}
+            rows={10}
+            onChange={handleChange}
+            onPaste={handlePaste}
+            value={input}
+          />
+          <br />
+          <br />
+          <Center>
+            <Button type="submit" disabled={input === ""}>
+              Submit
+            </Button>
+          </Center>
+        </form>
+        <Center>
+          {presId !== ""
+            ? toast({
+                title: "Hooray!!!",
+                description: `Presentation has been created! ${url}`,
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+              }) && setPresId("")
+            : null}
+        </Center>
       </Container>
     </div>
-
   );
 }
 
